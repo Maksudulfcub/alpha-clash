fd29a9177ca280ebb7d72770f67db2df822a93d6
Maksudulfcub <maaksuduldish@gmail.com>
2024-02-17 16:46:03 +0600

    All done

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -6,6 +6,53 @@
 //     playgroundSection.classList.remove('hidden');
 // }
 
+function handleKeyboardKeyUpEvent(event){
+    const playerPressed = event.key
+    console.log(playerPressed);
+
+    if(playerPressed === 'Escape'){
+        gameOver();
+    }
+
+    const currentAlphabetElement = document.getElementById('current-alphabet')
+    const currentAlphabet = currentAlphabetElement.innerText
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    console.log(playerPressed, expectedAlphabet);
+
+    if(playerPressed === expectedAlphabet){
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+        console.log(currentScore);
+
+        
+
+        const newScore = currentScore + 1;
+        currentScoreElement.innerText = newScore;
+
+        
+
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    }
+    else{
+        console.log('Opps ! You lost a life')
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+
+        const newLife = currentLife - 1;
+        currentLifeElement.innerText = newLife;
+
+        if(newLife === 0){
+            gameOver();
+        }
+    }
+}
+
+document.addEventListener('keyup', handleKeyboardKeyUpEvent);
+
+
 
 function continueGame(){
     const alphabet = getARandomAlphabet();
@@ -20,6 +67,22 @@ function continueGame(){
 
 function play(){
     hideElementById('home-screen');
+    hideElementById('final-score');
     showElementById('play-ground');
+
+    setTextElementValueById('current-life',5);
+    setTextElementValueById('current-score',0);
+
     continueGame()
+}
+
+function gameOver(){
+    hideElementById('play-ground');
+    showElementById('final-score');
+
+    const lastScore = getTextElementValueById('current-score')
+    setTextElementValueById('last-score', lastScore);
+
+    const currentAlphabet = getElementTextById('current-alphabet');
+    removeBackgroundColorById(currentAlphabet);
 }
